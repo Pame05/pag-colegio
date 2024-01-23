4b95cf311b729d5ee88007b8adf25ed86755739f
Alexnarea <lanarea@sudamericano.edu.ec>
2024-01-23 00:53:46 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeadComponent } from './components/head/head.component';
+import { BodyComponent } from './components/body/body.component';
+import { LegsComponent } from './components/legs/legs.component';
+import { FeetComponent } from './components/feet/feet.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeadComponent, BodyComponent, LegsComponent, FeetComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
